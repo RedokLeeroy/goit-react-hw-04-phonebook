7ac549a33d3f71572e80e90e3294e4d6e580dbc7
RedokLeeroy <Lys1999@ukr.net>
2022-07-28 17:15:05 +0300

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import { PhonebookForm } from "./PhonebookForm/PhonebookForm";
 import { Section } from "./Section/Section";
 import { nanoid } from 'nanoid'
@@ -7,66 +7,119 @@ import { FindByName } from "./FindByName/FindByName"
 
 
 
-export class App extends Component {
-  state= {
-  contacts: [  {id: 'id-1', name: 'Rosie Simpson', phone: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', phone: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', phone: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', phone: '227-91-26'}],
-    filter: ""
-  }
 
-  componentDidMount() {
-    const savedContacts = localStorage.getItem('localContacts')
-    if(savedContacts) {
-      this.setState({contacts:JSON.parse(savedContacts)})
-    } 
-  }
+export const App = () => {
+ const [filter, setFilter] = useState("")
+ const [contacts, setContacts] = useState(JSON.parse(localStorage.getItem('localContacts')) || [])
 
-  componentDidUpdate(prevProps, prevState) {
-    if(prevState.contacts !== this.state.contacts) {
-      localStorage.setItem("localContacts", JSON.stringify(this.state.contacts))
-    }
-  }
+    useEffect(()=> {localStorage.setItem('localContacts', JSON.stringify(contacts))},[contacts])
 
-    handlerSubmit = (contact) => { 
-      const isHere = this.state.contacts.some(({name}) => name === contact.name)
-
+    const handlerSubmit = (contact) => { 
+      const isHere = contacts.some(({name}) => name === contact.name)
+      const obj = {...contact, id: nanoid()}
       if (isHere) {
         alert(`Name already in contacts`)
       return         
       }
-      this.setState((ps) => ({ contacts: [...ps.contacts, {...contact, id: nanoid()}] }) )
+      setContacts(ps => [...ps, obj] )
         
     }
-    
-    handleChange = (event) => {
+
+    const handleChange = (event) => {
       const {value} = event.target
-      this.setState({filter : value})
+      setFilter(value)
     }
 
-    handleFilters = () => {
-          const {contacts, filter} = this.state
+    const handleFilters = () => {
       return contacts.filter((contact) => contact.name.toLowerCase().includes(filter.toLowerCase()))
     }
 
-    handleDelete = (id) =>{
-      this.setState((ps) => ({contacts :ps.contacts.filter((el) => el.id !== id)}))
+    const handleDelete = (id) =>{
+      setContacts((ps) => (ps.filter((el) => el.id !== id)))
     }
 
-  render() {
-    const {filter} = this.state
-    return(<>
-      <Section title= "phonebook">
-      <PhonebookForm onSubmit={this.handlerSubmit}/>
-      </Section>
-      <Section title= "Contacts">
-        <FindByName value={filter} onChange={this.handleChange} />
-        <Contacts contact= {this.handleFilters()} onDelete={this.handleDelete} />
-      </Section>
-      </> 
-    )
-  }
-}
+
+
+return (<>
+       <Section title= "phonebook">
+       <PhonebookForm onSubmit={handlerSubmit}/>
+       </Section>
+       <Section title= "Contacts">
+         <FindByName value={filter} onChange={handleChange} />
+         <Contacts contact= {handleFilters()} onDelete={handleDelete} />
+       </Section>
+       </>) 
+    }
+
+
+
+
+
+
+
+
+
+
+// export class App extends Component {
+//   state= {
+//   contacts: [  {id: 'id-1', name: 'Rosie Simpson', phone: '459-12-56'},
+//     {id: 'id-2', name: 'Hermione Kline', phone: '443-89-12'},
+//     {id: 'id-3', name: 'Eden Clements', phone: '645-17-79'},
+//     {id: 'id-4', name: 'Annie Copeland', phone: '227-91-26'}],
+//     filter: ""
+//   }
+
+//   componentDidMount() {
+//     const savedContacts = localStorage.getItem('localContacts')
+//     if(savedContacts) {
+//       this.setState({contacts:JSON.parse(savedContacts)})
+//     } 
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if(prevState.contacts !== this.state.contacts) {
+//       localStorage.setItem("localContacts", JSON.stringify(this.state.contacts))
+//     }
+//   }
+
+//     handlerSubmit = (contact) => { 
+//       const isHere = this.state.contacts.some(({name}) => name === contact.name)
+
+//       if (isHere) {
+//         alert(`Name already in contacts`)
+//       return         
+//       }
+//       this.setState((ps) => ({ contacts: [...ps.contacts, {...contact, id: nanoid()}] }) )
+        
+//     }
+    
+//     handleChange = (event) => {
+//       const {value} = event.target
+//       this.setState({filter : value})
+//     }
+
+//     handleFilters = () => {
+//           const {contacts, filter} = this.state
+//       return contacts.filter((contact) => contact.name.toLowerCase().includes(filter.toLowerCase()))
+//     }
+
+//     handleDelete = (id) =>{
+//       this.setState((ps) => ({contacts :ps.contacts.filter((el) => el.id !== id)}))
+//     }
+
+//   render() {
+//     const {filter} = this.state
+//     return(<>
+//       <Section title= "phonebook">
+//       <PhonebookForm onSubmit={this.handlerSubmit}/>
+//       </Section>
+//       <Section title= "Contacts">
+//         <FindByName value={filter} onChange={this.handleChange} />
+//         <Contacts contact= {this.handleFilters()} onDelete={this.handleDelete} />
+//       </Section>
+//       </> 
+//     )
+//   }
+// }
 
 
--- src/components/PhonebookForm/PhonebookForm.jsx
+++ src/components/PhonebookForm/PhonebookForm.jsx
@@ -1,40 +1,35 @@
-import { Component } from "react";
+import { useState } from "react";
 import { FormSubmit } from "./FormSubmit";
 import { NameInput } from "./NameInput";
 import { PhoneInput } from "./PhoneInput";
-import PropTypes from "prop-types"
 
-export class PhonebookForm extends Component {
-    state = {
-        name : "",
-        phone: ""
-    }
 
-    static propTypes = {
-    onSubmit: PropTypes.func
-    }
 
-    handlerInput = (event) => {
-        const {name, value} = event.target
-        return this.setState({[name] : value})
-    }
-
-    handSubmit= (event) => {
+export const PhonebookForm = ({onSubmit}) => {
+    const [name, setName] = useState("")
+    const [phone, setPhone] = useState("")
+    
+    const handSubmit= (event) => {
         event.preventDefault()
-        this.props.onSubmit(this.state)
-        this.setState({ name : "",
-        phone: ""})
+        onSubmit({name, phone})
+        setName("")
+        setPhone("")
     }
 
-render () {
-    const {name, phone} = this.state
+    const handlerInput = (event) => {
+        const {name, value} = event.target
+        if (name === "name") {
+        setName(value)
+        }else if (name === "phone"){
+        setPhone(value)
+        }
+    }
+    
     return (
-        <form onSubmit={this.handSubmit}>
-        <NameInput value={name} name="name" func={this.handlerInput}></NameInput>
-        <PhoneInput value={phone} name="phone" func={this.handlerInput}></PhoneInput>
+        <form onSubmit={handSubmit}>
+        <NameInput value={name} name="name" func={handlerInput}></NameInput>
+        <PhoneInput value={phone} name="phone" func={handlerInput}></PhoneInput>
         <FormSubmit title="Add contact"></FormSubmit>
         </form>
     )
 }
-}
-
